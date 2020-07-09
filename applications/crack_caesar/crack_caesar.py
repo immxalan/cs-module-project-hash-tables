15985189be953e811456ba3da0cd28bfce2afcfb
immxalan <aedocoding@gmail.com>
2020-07-09 12:58:54 -0700

    Crack, Histo, Lookup done for Day 3

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,29 @@
 
 # Your code here
 
+letter_count = {}
+final_hash = {}
+final_text = ""
+
+with open("applications/crack_caesar/ciphertext.txt", encoding="utf-8") as cipher_text:
+    cipher = cipher_text.read()
+top_letters = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+for letter in cipher:
+    if letter.isalpha():
+        if letter not in letter_count:
+            letter_count[letter] = 1
+        else:
+            letter_count[letter] += 1
+sort_list = list(letter_count.items())
+sort_list.sort(reverse=True, key = lambda item: item[1])
+print(sort_list)
+for i, element in enumerate(top_letters):
+    final_hash[sort_list[i][0]] = top_letters[i]
+
+for letter in cipher:
+    if letter.isalpha():
+        final_text += final_hash[letter]
+    else:
+        final_text += letter
+print(final_text)
--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,43 @@
 # Your code here
 
+f = open("applications/histo/robin.txt", "r")
+remove_chars = ["\"", ":", ";", ",", ".", "-", "+", "=", "/", "\\",
+               "|", "[", "]", "{", "}", "(", ")", "*", "^", "&", '', "!", "?"]
+space = " "
+
+cache = {}
+word = ""
+
+for x in f.read().lower():
+    if x in remove_chars:
+        continue
+
+    if x == space or x == "\n":
+        if word in cache:
+            cache[word] += 1
+        else:
+            cache[word] = 1
+        word = ""
+    else:
+        word += x
+
+
+print(cache)
+
+a = sorted(cache.items(), key=lambda keyAndVal: (keyAndVal[1], keyAndVal[0]))
+
+a = a[::-1]
+# print(a)
+str = ""
+number_of_spaces = 20
+
+for item in a:
+    str += item[0]
+
+    for i in range(0, number_of_spaces - len(str)):
+        str += " "
+
+    for i in range(0, item[1]):
+        str += "*"
+    print(str)
+    str = ""
